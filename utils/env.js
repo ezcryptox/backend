56d7edd7dc3a098f55989e5cbeb4a716a4ff8ac6
wizjamin <jaminwizzy@gmail.com>
2024-07-27 09:56:25 +0100

    Updated api keys with new moonpay acc

--- utils/env.js
+++ utils/env.js
@@ -1,6 +1,6 @@
-const MOONPAY_PUBLIC_KEY = 'pk_test_cEHJDpoesWksBA3txcJvn58qSqyOvOl'; //TODO Change to LIVE KEY
-const MOONPAY_SECRET_KEY = 'sk_test_BFzNVO1YQUEOVOCXkxDkPXxZ2wKIxw'; //TODO Change to LIVE KEY
-const MOONPAY_WEBHOOK_SECRET = 'wk_test_XEyTPPRjPu66VhK5RAlNqkcicDeCzUn0'; //TODO Change to LIVE KEY
+const MOONPAY_PUBLIC_KEY = 'pk_test_zN6fst3pHTfKPewrR9gmoZL3eD8NfA'; //TODO Change to LIVE KEY
+const MOONPAY_SECRET_KEY = 'sk_test_6bLyRMVB5gQqRLAs5HQnlbiKw4Gw0bq1'; //TODO Change to LIVE KEY
+const MOONPAY_WEBHOOK_SECRET = 'wk_test_BQ655i6gr6v1XlNToSJwH2V0gFIArvk'; //TODO Change to LIVE KEY
 const TATUM_API_KEY = 't-6699a7552d63e7001c92f4ce-f23285d7138e466d96ba13a1'; //TODO Change to LIVE KEY
 const TATUM_WEBHOOK_SECRET = '3fc70e77d96add78fdb7d02b5b78048503c7fad16c787540a29e7c3c1369174b'; //TODO Reauth With LIVE KEY
 //const ASSET_WEBHOOK_URL = 'https://ezcryptox.onrender.com/api/assets/webhook'; // Uncomment for LIVE url
