17d36e40d8ed678e76d2260f7ad759d5f566460f
wizjamin <jaminwizzy@gmail.com>
2024-07-27 23:38:36 +0100

    Updates

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ app.use('/api/article', articleRoutes);
 app.get("/", (req, res) => {
     res.status(200).json({ status: "Server is running smoothly" })
 })
-// const dbHost = "highscoreteh"
-// const dbPass = "eNiIQbm4ZMSor8VL"
-// const dbCompany = "ezcryptox"
+const dbHost = "highscoreteh"
+const dbPass = "eNiIQbm4ZMSor8VL"
+const dbCompany = "ezcryptox"
 
 // const dbUri = `mongodb://127.0.0.1:27017/ezcryptox`
 const dbUri = `mongodb+srv://${dbHost}:${dbPass}@cluster0.xmpkpjc.mongodb.net/${dbCompany}?retryWrites=true&w=majority`
