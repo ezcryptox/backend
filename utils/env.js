8b2c21ab0a552768ca95f36e8498ec5c80ae2eaf
wizjamin <jaminwizzy@gmail.com>
2024-07-28 04:58:45 +0100

    Updates

--- utils/env.js
+++ utils/env.js
@@ -3,8 +3,7 @@ const MOONPAY_SECRET_KEY = 'sk_test_6bLyRMVB5gQqRLAs5HQnlbiKw4Gw0bq1'; //TODO Ch
 const MOONPAY_WEBHOOK_SECRET = 'wk_test_BQ655i6gr6v1XlNToSJwH2V0gFIArvk'; //TODO Change to LIVE KEY
 const TATUM_API_KEY = 't-6699a7552d63e7001c92f4ce-f23285d7138e466d96ba13a1'; //TODO Change to LIVE KEY
 const TATUM_WEBHOOK_SECRET = '3fc70e77d96add78fdb7d02b5b78048503c7fad16c787540a29e7c3c1369174b'; //TODO Reauth With LIVE KEY
-//const ASSET_WEBHOOK_URL = 'https://ezcryptox.onrender.com/api/assets/webhook'; // Uncomment for LIVE url
-const ASSET_WEBHOOK_URL = 'https://webhook.site/e4601097-a11e-4d4c-b698-c835693070c9'; //TODO Change to LIVE url
+const ASSET_WEBHOOK_URL = 'https://ezcryptox.onrender.com/api/assets/webhook';
 
 module.exports = {
   MOONPAY_PUBLIC_KEY, MOONPAY_SECRET_KEY, TATUM_API_KEY, TATUM_WEBHOOK_SECRET, ASSET_WEBHOOK_URL, MOONPAY_WEBHOOK_SECRET
